1bcdbe7f10b2bef0b25701afd13f56d92aa9430b
nippysky <nsinfo247@gmail.com>
2025-07-31 00:10:56 +0100

    retry

--- prisma.config.js
+++ prisma.config.js
@@ -1,10 +1,10 @@
-// prisma.config.js
-/**
- * @type {import('prisma').PrismaConfig}
- */
-module.exports = {
+// prisma.config.ts
+import { defineConfig } from "prisma";
+
+export default defineConfig({
   schemaPath: "prisma/schema.prisma",
+  // tell Prisma how to seed your database
   seed: {
     run: "node prisma/seed.cjs",
   },
-};
+});
